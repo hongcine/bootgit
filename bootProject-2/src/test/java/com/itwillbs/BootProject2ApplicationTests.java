18726a807b6242e7e3ae9a606cab935e8b1b8df7
hongcine <hongcine@naver.com>
2025-06-02 12:36:24 +0900

    boot project

---
+++ bootProject-2/src/test/java/com/itwillbs/BootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwillbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
